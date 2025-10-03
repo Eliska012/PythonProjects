14cca70755ad80d7be261ff5fff130fcdb65b381
D32\zak <spseplzen@spseplzen.cz>
2025-10-03 12:26:35 +0200

    prvni

--- main.py
+++ main.py
@@ -1,2 +1,50 @@
 print("Hello world")
-print("ahoj")
+
+a = "ahoj"
+b = a
+
+print(b) # komentář
+#komemtář
+
+# datové typy v pytohnu
+
+samopopisne_nazvy_promenych = "ahoj"
+print(samopopisne_nazvy_promenych)
+print(type(samopopisne_nazvy_promenych))
+cislo = 9
+print (cislo)
+print(type(cislo))
+
+desitinne_cislo = 5.5
+print(desitinne_cislo)
+print(type(desitinne_cislo))
+
+vysledek = 5/7
+print(vysledek)
+print(type(vysledek))
+
+a = ("ahoj")
+a = 5
+
+#cisla
+
+a = 5
+b = 7
+c = a + b
+d = a - b
+e = a * b
+f = a / b #b!=0
+g = a ** 2
+h = a ** (1/2)
+i = b  % a #zbytek po celociselnym deleni
+y =  5 + 5 * 2
+
+print(c)
+
+x = 5.7
+print(round(x))
+print(int(x))
+print(i)
+
+logicka = True
+l2 = False
