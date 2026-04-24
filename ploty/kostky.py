806e8081abb0071ef5cb15a9019b364a245e08fb
D32\zak <spseplzen@spseplzen.cz>
2026-04-24 12:22:04 +0200

    kostky2

--- ploty/kostky.py
+++ ploty/kostky.py
@@ -31,3 +31,15 @@ def nez_padne(n):
     return vysledky
 print(nez_padne(100))
 print(nez_padne(50))
+
+def simulace(n):
+    pole = [0]*20
+    for i in range(n):
+        attempt = pokus()
+        if attempt > 20:
+            attempt = 20
+        pole[attempt -1]+=1
+    return pole
+
+print(simulace(100))
+print(simulace(50))
